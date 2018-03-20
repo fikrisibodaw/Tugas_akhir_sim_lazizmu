bc9af8c9924aec3afd0ceed995885c510d81cdf1
fikrisibodaw <fikribodaw2010@gmail.com>
2018-03-20 17:04:03 +0700

    lagi

--- app/districts.php
+++ app/districts.php
@@ -8,26 +8,3 @@ class districts extends Model
 {
     //aaasdasdasdasdasd
 }
-
-
-<?php
-
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class districts extends Model
-{
-    //aaasdasdasdasdasd
-}
-
-<?php
-
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class districts extends Model
-{
-    //aaasdasdasdasdasd
-}
